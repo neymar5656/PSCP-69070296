606218ec17868add410d3eeab0c0ec2e59dd5fe6
neymar5656 <69070296@kmitl.ac.th>
2026-07-24 19:36:02 +0700

    Update 3020.py

--- 3020.py
+++ 3020.py
@@ -5,5 +5,9 @@ c = int(input())
 d = int(input())
 
 a1 = (d - ((d//b))) 
-cal = a1*a +((d/b)* c)
-print(int(cal))
+cal = a1*a +((d//b)* c)
+
+if not d%b:
+    print(int(cal + 1 ))
+else:
+    print(int(cal))
