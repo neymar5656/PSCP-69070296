ff2dfb2782682602c28a23efddde423ddf0d2b8e
neymar5656 <69070296@kmitl.ac.th>
2026-08-06 21:12:33 +0700

    Update 3014.py

--- 3014.py
+++ 3014.py
@@ -1,17 +1,13 @@
 """humlek"""
-a, b, c, d = ((int(input()))for _ in range(4))
+a, b, c, d = (int(input()) for _ in range(4))
 
-real_price = d//a
+bottles = d // a
+caps = bottles
 
-if not b or not c:
-    print(real_price)
-elif b == 1:
-    E = real_price//b
-    E = E*c
-    sol = real_price + E
-    print(sol- 1)
-else:
-    E = real_price//b
-    E = E*c
-    sol = real_price + E
-    print(sol)
+if b > 0:
+    while caps >= b:
+        exchanges = caps // b
+        got = exchanges * c
+        bottles += got
+        caps = caps % b + got
+print(bottles)
