2b851745421e507a9be62a3439f3097e5f1f13ab
neymar5656 <69070296@kmitl.ac.th>
2026-08-10 20:05:44 +0700

    wte

---
+++ 3034.py
@@ -0,0 +1,5 @@
+"""pod"""
+pod= input().split(" ")
+G = int(pod[0])
+for i in range(G):
+    print(i)
--- Surprise.py
+++ Surprise.py
@@ -2,9 +2,8 @@
 num1 = float(input())
 num2 = float(input())
 
-n = num1 - 2 * num2
+n = max(num1 - 2 * num2)
 print(n)
-print(num2 - n)
 if num2 - n > 2.0 :
     print('Surprising')
 else:
