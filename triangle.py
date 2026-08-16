9c7fe5f59dfd0ba97f6ec32819b3c20f00398428
neymar5656 <69070296@kmitl.ac.th>
2026-08-16 22:16:36 +0700

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,8 +1,12 @@
-"""samriam"""
-
-i = 0
-while i < 3:
-    i += 1
-    if i == 2:
-        continue
-    print(i)
+"""I LOVE MY JOB"""
+def main():
+    """I LOVE YOUNGGU"""
+    a = int(input())
+    i = 1
+    for i in range(a):
+        if i in range(1,3):
+            print("0" * i)
+        elif 2 < i < a:
+            print(f"0{'1' * (i - 2)}0")
+    print("0" * a)
+main()
