9ff70b27716bcdd0e1d0f5263822b8e897176ee8
neymar5656 <69070296@kmitl.ac.th>
2026-08-26 21:54:51 +0700

    Update brick.py

--- brick.py
+++ brick.py
@@ -4,13 +4,7 @@ b = int(input())
 goal = int(input())
 
 b = b*5
+all = a+b
 
-if a + b < goal:
-    print(-1)
-elif not b - goal:
-    print(0)
-elif b > goal:
-    if b%goal and a :
-        print(0)
-    elif not b%goal and a>5:
-        print(b)
+if b > goal:
+    print('hee')
